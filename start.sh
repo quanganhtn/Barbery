0bcb44309c590855e2c03eaf30d2a83d5d4572d4
Quang Anh <quanganhtn@gmail.com>
2026-04-25 21:19:33 +0700

    Clear Laravel views on startup

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ echo "Prepare Laravel..."
 php artisan key:generate --force || true
 php artisan config:clear || true
 php artisan cache:clear || true
+php artisan view:clear || true
 # php artisan migrate --force || true
 
 echo "Start Laravel server..."
